ae24713054533afe17d2033f808bb93f109bed56
Andrey <hhhandrej69@gmail.com>
2020-06-08 11:10:49 +0300

    add types

--- src/handlers/types.py
+++ src/handlers/types.py
@@ -1,10 +1,18 @@
 from src.handlers.handler import Handler
-
+from src.utils import find_closest, send_place
 
 class TypesHandler(Handler):
 
     def activate(self):
         self.add_handler(self.photo, content_types=['photo'])
+        self.add_handler(self.location, content_types=['location'])
 
     def photo(self, m):
         print(m.photo[0].file_id)
+
+    def location(self, m):
+        lat_user = m.location.latitude
+        long_user = m.location.longitude
+        ids = find_closest(lat_user, long_user, 5)
+        for place_id in ids:
+            send_place(self.bot, m.chat.id, place_id)
